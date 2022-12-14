d28c471e4b0bd3c2a1f4e7277b9f911fe7c40b6c
nourobeid21 <nfo02@mail.aub.edu>
2022-12-14 18:38:03 +0200

    continued the authentication part

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,85 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using _278Project.Models;
+using _278Project.Repos;
+using Microsoft.AspNetCore.Identity;
+
+namespace _278Project.Controllers;
+
+public class AccountController : Controller
+{   private UserManager<User> userManager;
+    private SignInManager<User> signInManager;
+
+
+    public AccountController(UserManager<User> userMngr, SignInManager<User> signInMngr  )
+    {
+        userManager= userMngr;
+        signInManager= signInMngr;
+    }
+    [HttpGet]
+    public IActionResult Register()
+    {
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterViewModel model)
+    {
+        if(ModelState.IsValid){
+            var user= new User{Username= model.Username};
+            var result= await userManager.CreateAsync(user, model.Password);
+            if(result.Succeeded){
+                await signInManager.SignInAsync(user, isPersistent:false);
+                return RedirectToAction("Index", "Home");
+            }
+            else{
+                foreach(var error in result.Errors){
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+        }
+        return View(model);
+    }
+
+     [HttpGet]
+    public IActionResult LogIn(string returnURL = "")
+    {
+        var model = new LoginViewModel { ReturnUrl = returnURL };
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> LogIn(LoginViewModel model)
+    {
+        if (ModelState.IsValid) {
+        var result = await signInManager.PasswordSignInAsync(
+        model.Username, model.Password,
+        isPersistent: model.RememberMe,
+        lockoutOnFailure: false);
+        if (result.Succeeded) {
+        if (!string.IsNullOrEmpty(model.ReturnUrl) &&
+        Url.IsLocalUrl(model.ReturnUrl))
+        {
+        return Redirect(model.ReturnUrl);
+        }
+        else
+        {
+        return RedirectToAction("Index", "Home");
+        }
+        }
+        }
+        ModelState.AddModelError("", "Invalid username/password.");
+        return View(model);
+    }
+    [HttpPost]
+     public async Task<IActionResult> LogOut()
+    {
+        await signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace _278Project.Models;
+public class RegisterViewModel{
+    [Required(ErrorMessage= "Please enter a username.")]
+    public string Username {get;set;}
+    [Required(ErrorMessage= "Please enter a password.")]
+    [Compare("ConfirmPassword")]
+    [DataType(DataType.Password)]
+    public string Password{get;set;}
+    [Required(ErrorMessage= "Please confirm your password.")]
+    [DataType(DataType.Password)]
+    [Display(Name= "Confirm Password")]
+    public string ConfirmPassword{get;set;}
+    [Required(ErrorMessage= "Please enter a valid email.")]
+    public string Email{get;set;}
+    public int PhoneNumber{get;set;}
+
+
+}
